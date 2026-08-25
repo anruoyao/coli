50da37ce6b13ab6f052ca082fe8c3467caa092c4
Your Name <2195299055@qq.com>
2026-08-25 13:26:10 +0800

    feat: 移动端 Web 首页同步新增关注/推荐双时间线切换

--- resources/js/spa/apps/mobile/views/home/HomeIndex.vue
+++ resources/js/spa/apps/mobile/views/home/HomeIndex.vue
@@ -1,5 +1,18 @@
 <template>
 	<TimelineContainer>
+        <!-- 首页时间线切换：关注 / 推荐（吸顶） -->
+        <div class="sticky top-0 popup-background-tr z-10">
+            <ContentTabs v-bind:cols="2">
+                <TabsItem v-bind:isActive="state.activeTab === 0" v-on:click="switchTab(0)">
+                    {{ $t('labels.following') }}
+                </TabsItem>
+                <TabsItem v-bind:isActive="state.activeTab === 1" v-on:click="switchTab(1)">
+                    {{ $t('labels.for_you') }}
+                </TabsItem>
+            </ContentTabs>
+            <Border></Border>
+        </div>
+
         <div class="px-4 pb-3 pt-1">
             <StoriesFeed></StoriesFeed>
         </div>
@@ -8,36 +21,76 @@
 			<TimelinePublicationSkeleton v-for="i in 2" v-bind:key="i"></TimelinePublicationSkeleton>
 		</div>
 		<div class="pb-6" v-else>
-            <FeedUpdate v-if="timelineNewPosts.length" v-bind:posts="timelineNewPosts" v-on:click="applyTimelineUpdate"></FeedUpdate>
-			<div v-if="timelinePosts.length">
-                <template v-for="(postData, index) in timelinePosts" v-bind:key="postData.hash_id">
-                    <TimelinePublication v-bind:postData="postData" v-on:delete="handlePostDelete(postData)"></TimelinePublication>
-                    
-                    <!-- Show follow recommendation every 10 posts -->
-                    <template v-if="(index + 1) % 35 === 0">
-                        <FollowRecommendation v-bind:key="index"></FollowRecommendation>
+            <template v-if="state.activeTab === 0">
+                <FeedUpdate v-if="timelineNewPosts.length" v-bind:posts="timelineNewPosts" v-on:click="applyTimelineUpdate"></FeedUpdate>
+                <div v-if="timelinePosts.length">
+                    <template v-for="(postData, index) in timelinePosts" v-bind:key="postData.hash_id">
+                        <TimelinePublication v-bind:postData="postData" v-on:delete="handlePostDelete(postData)"></TimelinePublication>
+                        
+                        <!-- Show follow recommendation every 35 posts -->
+                        <template v-if="(index + 1) % 35 === 0">
+                            <FollowRecommendation v-bind:key="index"></FollowRecommendation>
+                        </template>
+
+                        <!-- Show ad card every 10 posts -->
+                        <template v-if="(index + 1) % 10 === 0">
+                            <AdCard v-bind:key="index"></AdCard>
+                            <Border height="h-2" opacity="opacity-30"></Border>
+                        </template>
                     </template>
 
-                    <!-- Show ad card every 10 posts -->
-                    <template v-if="(index + 1) % 10 === 0">
-                        <AdCard v-bind:key="index"></AdCard>
-                        <Border height="h-2" opacity="opacity-30"></Border>
-                    </template>
+                    <div v-if="state.isLoadingContent">
+                        <div class="flex justify-center my-4">
+                            <div class="colibri-primary-animation"></div>
+                        </div>
+                    </div>
+                </div>
+                <div v-else>
+                    <div class="py-32">
+                        <p class="text-lab-sc text-par-s text-center">
+                            {{ $t('empty_state.home.posts') }}
+                        </p>
+                    </div>
+                </div>
+            </template>
+
+            <template v-else>
+                <div v-if="state.isRecommendLoading">
+                    <TimelinePublicationSkeleton v-for="i in 5" v-bind:key="i"></TimelinePublicationSkeleton>
+                </div>
+                <template v-else>
+                    <FeedUpdate v-if="recommendedNewPosts.length" v-bind:posts="recommendedNewPosts" v-on:click="applyRecommendedUpdate"></FeedUpdate>
+                    <div v-if="recommendedPosts.length">
+                        <template v-for="(postData, index) in recommendedPosts" v-bind:key="postData.id">
+                            <TimelinePublication v-bind:postData="postData" v-on:delete="handlePostDelete(postData)"></TimelinePublication>
+
+                            <!-- Show follow recommendation every 35 posts -->
+                            <template v-if="(index + 1) % 35 === 0">
+                                <FollowRecommendation v-bind:key="index"></FollowRecommendation>
+                            </template>
+
+                            <!-- Show ad card every 10 posts -->
+                            <template v-if="(index + 1) % 10 === 0">
+                                <AdCard v-bind:key="index"></AdCard>
+                                <Border height="h-2" opacity="opacity-30"></Border>
+                            </template>
+                        </template>
+
+                        <div v-if="state.isLoadingContent">
+                            <div class="flex justify-center my-4">
+                                <div class="colibri-primary-animation"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-else>
+                        <div class="py-32">
+                            <p class="text-lab-sc text-par-s text-center">
+                                {{ $t('empty_state.empty') }}
+                            </p>
+                        </div>
+                    </div>
                 </template>
-
-				<div v-if="state.isLoadingContent">
-					<div class="flex justify-center my-4">
-						<div class="colibri-primary-animation"></div>
-					</div>
-				</div>
-			</div>
-			<div v-else>
-				<div class="py-32">
-					<p class="text-lab-sc text-par-s text-center">
-						{{ $t('empty_state.home.posts') }}
-					</p>
-				</div>
-			</div>
+            </template>
 		</div>
 	</TimelineContainer>
 </template>
@@ -45,6 +98,7 @@
 <script>
     import { defineComponent, ref, reactive, onMounted, computed, onUnmounted } from 'vue';
     import { useTimelineStore } from '@M/store/timeline/timeline.store.js';
+    import { useExplorePostsStore } from '@M/store/explore/posts.store.js';
     import { useDeletePost } from '@/kernel/vue/composables/delete-post/index.js';
     import { useInfiniteScroll } from '@/kernel/vue/composables/infinite-scroll/index.js';
     import { colibriEventBus } from '@/kernel/events/bus/index.js';
@@ -56,6 +110,8 @@
     import AdCard from '@M/components/ads/AdCard.vue';
     import FollowRecommendation from '@M/components/recommend/follow/FollowRecommendation.vue';
     import FeedUpdate from '@M/components/timeline/update/FeedUpdate.vue';
+    import ContentTabs from '@M/components/general/tabs/content/ContentTabs.vue';
+    import TabsItem from '@M/components/general/tabs/content/parts/TabsItem.vue';
 
     export default defineComponent({
         setup: function() {
@@ -64,9 +120,9 @@
                 isLoadingContent: false,
                 noMoreContent: false,
                 isUpdating: false,
-                filter: {
-                    page: 1
-                }
+                activeTab: 0,
+                isRecommendLoading: false,
+                noMoreRecommend: false
             });
 
             let updateIntervalId = null;
@@ -75,6 +131,7 @@
             const { postDeleter } = useDeletePost();
 
             const timelineStore = useTimelineStore();
+            const explorePostsStore = useExplorePostsStore();
 
             const timelineNewPosts = computed(() => {
                 return timelineStore.update;
@@ -82,6 +139,14 @@
 
             const timelinePosts = computed(() => {
                 return timelineStore.posts;
+            });
+
+            const recommendedNewPosts = computed(() => {
+                return explorePostsStore.update;
+            });
+
+            const recommendedPosts = computed(() => {
+                return explorePostsStore.posts;
             });
 
             onMounted(async () => {
@@ -92,33 +157,40 @@
                 
                 if(! timelinePosts.value.length) {
                     await timelineStore.initialLoad();
-    
-                    state.isLoading = false;
+                }
 
-                    // Update feed every 10 minutes.
-                    // 10 minutes are optimal for the feed update interval.
-                    
-                    updateIntervalId = setInterval(async () => {
-                        if(! state.isUpdating) {
-                            if(timelineNewPosts.value.length == 0 && updateAttempts > 10) {
-                                clearInterval(updateIntervalId);
+                state.isLoading = false;
+
+                // Update feed every 10 minutes.
+                // 10 minutes are optimal for the feed update interval.
+                
+                updateIntervalId = setInterval(async () => {
+                    if(! state.isUpdating) {
+                        const activeNewPosts = (state.activeTab === 0) ? timelineNewPosts.value : recommendedNewPosts.value;
+
+                        if(activeNewPosts.length == 0 && updateAttempts > 10) {
+                            clearInterval(updateIntervalId);
+                        }
+                        else {
+                            state.isUpdating = true;
+
+                            if(state.activeTab === 0) {
+                                if(timelinePosts.value.length) {
+                                    await timelineStore.updateFeed();
+                                }
                             }
                             else {
-                                state.isUpdating = true;
-        
-                                await timelineStore.updateFeed();
-        
-                                state.isUpdating = false;
-
-                                updateAttempts++;
+                                if(recommendedPosts.value.length) {
+                                    await explorePostsStore.updateFeed();
+                                }
                             }
-                        }
-                    }, ((60 * 1000) * 10));
-                }
 
-                else{
-                    state.isLoading = false;
-                }
+                            state.isUpdating = false;
+
+                            updateAttempts++;
+                        }
+                    }
+                }, ((60 * 1000) * 10));
             });
 
             onUnmounted(() => {
@@ -127,27 +199,64 @@
                 }
 			});
 
+            const loadRecommended = async () => {
+                state.isRecommendLoading = true;
+
+                explorePostsStore.resetFilter();
+
+                if(! recommendedPosts.value.length) {
+                    await explorePostsStore.fetchPosts();
+                }
+
+                state.isRecommendLoading = false;
+            };
+
+            const switchTab = (tabIndex) => {
+                if(state.activeTab === tabIndex) {
+                    return;
+                }
+
+                state.activeTab = tabIndex;
+
+                if(tabIndex === 1 && ! recommendedPosts.value.length) {
+                    loadRecommended();
+                }
+            };
+
             const loadMorePost = async () => {
 				try {
-					if(! state.isLoadingContent && ! state.noMoreContent && timelinePosts.value.length) {
-						state.isLoadingContent = true;
+					if(state.activeTab === 0) {
+						if(! state.isLoadingContent && ! state.noMoreContent && timelinePosts.value.length) {
+							state.isLoadingContent = true;
 
-						await timelineStore.loadNextPage().then(function(response) {
-							let content = response.data.data;
+							await timelineStore.loadNextPage().then(function(response) {
+								let content = response.data.data;
 
-							if(content.length) {
-								timelineStore.appendPosts(content);
-							}
-							else{
-								state.noMoreContent = true;
-							}
-						}).catch((error) => {
-							if(error.response) {
-								state.noMoreContent = true;
-							}
-						});
+								if(content.length) {
+									timelineStore.appendPosts(content);
+								}
+								else{
+									state.noMoreContent = true;
+								}
+							}).catch((error) => {
+								if(error.response) {
+									state.noMoreContent = true;
+								}
+							});
 
-						state.isLoadingContent = false;
+							state.isLoadingContent = false;
+						}
+					}
+					else {
+						if(! state.isLoadingContent && ! state.noMoreRecommend && recommendedPosts.value.length) {
+							state.isLoadingContent = true;
+
+							explorePostsStore.filter.page += 1;
+
+							state.noMoreRecommend = (! await explorePostsStore.loadMorePosts());
+
+							state.isLoadingContent = false;
+						}
 					}
 				} catch (error) {
 					console.log(error);
@@ -162,6 +271,9 @@
                 timelinePosts: timelinePosts,
                 state: state,
                 timelineNewPosts: timelineNewPosts,
+                recommendedPosts: recommendedPosts,
+                recommendedNewPosts: recommendedNewPosts,
+                switchTab: switchTab,
                 handlePostDelete: (postData) => {
                     postDeleter(postData, (postId) => {
                         colibriEventBus.emit('timeline:post-deleted', postId);
@@ -171,6 +283,9 @@
                 },
                 applyTimelineUpdate: () => {
                     timelineStore.applyUpdate();
+                },
+                applyRecommendedUpdate: () => {
+                    explorePostsStore.applyUpdate();
                 }
             };
         },
@@ -181,7 +296,9 @@
             StoriesFeed: StoriesFeed,
             AdCard: AdCard,
             FollowRecommendation: FollowRecommendation,
-            FeedUpdate: FeedUpdate
+            FeedUpdate: FeedUpdate,
+            ContentTabs: ContentTabs,
+            TabsItem: TabsItem
         }
     });
 </script>
